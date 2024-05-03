ba02ce5c9180d66e79de80547e0f298f475857c7
OscGl <oscar.gledel@univ-tlse3.fr>
2024-05-03 16:55:35 +0200

    fin de l'interface InfoJoueur, ajout de la sous-classe Effet qui est directement integree a InfoJoueur. Verification des variables et des imports inutilisés

--- src/main/java/boundary/InfosJoueur.java
+++ src/main/java/boundary/InfosJoueur.java
@@ -4,11 +4,6 @@
  */
 package boundary;
 
-import java.awt.BasicStroke;
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-
 /**
  *
  * @author ogled
@@ -42,6 +37,20 @@ public class InfosJoueur extends javax.swing.JPanel {
         // Pour l'édition dans le GUI
         firePropertyChange("PV", null, PV);
     }
+    
+    public void setDureeEffet(int duree) {
+        this.effet.setDureeEffet(duree);
+        
+        // Pour l'édition dans le GUI
+        firePropertyChange("effetDuree", null, duree);
+    }
+    
+    public void setEffet(Effet.TypeEffet effet) {
+        this.effet.setEffet(effet);
+        
+        // Pour l'édition dans le GUI
+        firePropertyChange("effet", null, effet);
+    }
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -54,6 +63,7 @@ public class InfosJoueur extends javax.swing.JPanel {
 
         background = new boundary.IconJoueur();
         pointsVie = new boundary.barreDeVie();
+        effet = new boundary.Effet();
 
         setPreferredSize(new java.awt.Dimension(306, 306));
 
@@ -72,38 +82,45 @@ public class InfosJoueur extends javax.swing.JPanel {
             .addGap(0, 32, Short.MAX_VALUE)
         );
 
+        effet.setEffet(boundary.Effet.TypeEffet.none);
+
         javax.swing.GroupLayout backgroundLayout = new javax.swing.GroupLayout(background);
         background.setLayout(backgroundLayout);
         backgroundLayout.setHorizontalGroup(
             backgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(backgroundLayout.createSequentialGroup()
                 .addGap(110, 110, 110)
-                .addComponent(pointsVie, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(180, Short.MAX_VALUE))
+                .addGroup(backgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(effet, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(pointsVie, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(207, Short.MAX_VALUE))
         );
         backgroundLayout.setVerticalGroup(
             backgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(backgroundLayout.createSequentialGroup()
                 .addGap(115, 115, 115)
                 .addComponent(pointsVie, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(128, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(effet, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(89, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(background, javax.swing.GroupLayout.DEFAULT_SIZE, 450, Short.MAX_VALUE)
+            .addComponent(background, javax.swing.GroupLayout.DEFAULT_SIZE, 477, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(background, javax.swing.GroupLayout.DEFAULT_SIZE, 275, Short.MAX_VALUE)
+            .addComponent(background, javax.swing.GroupLayout.DEFAULT_SIZE, 306, Short.MAX_VALUE)
         );
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private boundary.IconJoueur background;
+    private boundary.Effet effet;
     private boundary.barreDeVie pointsVie;
     // End of variables declaration//GEN-END:variables
 }
